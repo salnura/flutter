673ae02c74c874b80903832e914fb3cc352a8dc5
salnura <91246335+salnura@users.noreply.github.com>
2022-03-29 09:48:41 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/row_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Aplikasi Flutter Pertama",
+      home: RowWidget(),
+    );
+  }
+
+}
+
